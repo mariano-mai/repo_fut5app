5b221400c681a0a71e06f144cf2e38e8359f4026
Mariano <mariano.ramaidana@gmail.com>
2023-06-05 01:47:51 -0300

    creación de repositorio con proyecto maven

---
+++ fut5app/src/main/java/com/info/fut5app/Fut5appApplication.java
@@ -0,0 +1,13 @@
+package com.info.fut5app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fut5appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fut5appApplication.class, args);
+	}
+
+}
---
+++ fut5app/src/test/java/com/info/fut5app/Fut5appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.fut5app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fut5appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

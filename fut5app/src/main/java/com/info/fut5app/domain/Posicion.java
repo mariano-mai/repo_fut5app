acae1554c25c78cfd7b4c7551a1cb5ec7520533f
Mariano <mariano.ramaidana@gmail.com>
2023-06-05 17:07:33 -0300

    empecé a agregar funcionalidades

--- fut5app/src/main/java/com/info/fut5app/domain/Posicion.java
+++ fut5app/src/main/java/com/info/fut5app/domain/Posicion.java
@@ -1,8 +1,12 @@
 package com.info.fut5app.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Posicion {
 	
 	String nombreDePosicion;
+	List<Jugador> jugadores = new ArrayList<Jugador>();
 	
 	public String asignePosicion(String posicion) {
 		return nombreDePosicion;
---
+++ fut5app/src/main/java/com/info/fut5app/service/crear/AgregarEntrenador.java
@@ -0,0 +1,9 @@
+package com.info.fut5app.service.crear;
+
+public class AgregarEntrenador {
+	
+	public void crearEntrenador(Entrenador entrenador) {
+		
+	}
+
+}
